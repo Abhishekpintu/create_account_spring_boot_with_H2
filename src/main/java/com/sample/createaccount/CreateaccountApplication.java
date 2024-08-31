87a4bfb1bd88fb7729a00df1ccdb86c1a5a23195
Abhisheka Gopal <abhishekagopal@TMPL-LP-177s-MacBook-Pro.local>
2024-08-31 11:33:51 +0530

    intial commit

---
+++ src/main/java/com/sample/createaccount/CreateaccountApplication.java
@@ -0,0 +1,13 @@
+package com.sample.createaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateaccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/createaccount/CreateaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.createaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
